e8fb75cb3c3936ac3f5d3a465d089cd9082a10b3
faraway <patrickyyao@gmail.com>
2021-02-18 23:05:45 -0800

    add description

--- add_two_numbers.go
+++ add_two_numbers.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+/**
+You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+Example:
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+ */
 type listNode struct {
 	val int
 	next *listNode //pointer to the next node. Simply 'listNode' won't work
