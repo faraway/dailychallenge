cd532d8d094eb4638b1965b54d64a8e179da6f24
faraway <patrickyyao@gmail.com>
2021-02-21 22:07:43 -0800

    add template

---
+++ _template.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/**
+TODO: Description
+
+*/
+
+func main() {
+	fmt.Println("initializing test data...")
+	//TODO: Test cases
+	fmt.Println("Answer is:", yourSolutionFunc("((()))")) //True
+	fmt.Println("Answer is:", yourSolutionFunc("[()]{}")) //True
+	fmt.Println("Answer is:", yourSolutionFunc("({[)]")) //False
+}
+
+//TODO: fill your solution
+func yourSolutionFunc(input string) bool {
+
+	return true
+}
