1a1bdeb3d058a40a2033dd721a1ea857e08fd34e
faraway <patrickyyao@gmail.com>
2021-03-04 23:39:19 -0800

    add math util

--- utils/math.go
+++ utils/math.go
@@ -1,1 +1,21 @@
 package utils
+
+func MIN(num ...int) int {
+	min := num[0]
+	for _, x := range num {
+		if x < min {
+			min = x
+		}
+	}
+	return min
+}
+
+func MAX(num ...int) int {
+	max := num[0]
+	for _, x := range num {
+		if x > max {
+			max = x
+		}
+	}
+	return max
+}
