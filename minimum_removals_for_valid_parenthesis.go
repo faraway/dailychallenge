607bec0aa6a5e7c06f7ade7a4a52d801d97b607e
faraway <patrickyyao@gmail.com>
2021-04-13 00:14:19 -0700

    Add leet code variation

--- minimum_removals_for_valid_parenthesis.go
+++ minimum_removals_for_valid_parenthesis.go
@@ -16,6 +16,8 @@ Example:
 
 The above input should return 1.
 
+** ALSO SEE BELOW WITH A LEET CODE VARIATION **
+
 */
 
 func main() {
@@ -28,6 +30,11 @@ func main() {
 	fmt.Println("Answer is:", minimumRemovalsForValidParenthesis("((())")) //1
 	fmt.Println("Answer is:", minimumRemovalsForValidParenthesis("(()))(()")) //2
 	fmt.Println("Answer is:", minimumRemovalsForValidParenthesis("(())()()))")) //2
+
+	fmt.Println("Answer is:", leetcode_minimumRemovalsForValidParenthesis("lee(t(c)o)de)")) //"lee(t(c)o)de"
+	fmt.Println("Answer is:", leetcode_minimumRemovalsForValidParenthesis("a)b(c)d")) //"ab(c)d"
+	fmt.Println("Answer is:", leetcode_minimumRemovalsForValidParenthesis("))((")) //""
+	fmt.Println("Answer is:", leetcode_minimumRemovalsForValidParenthesis("(a(b(c)d)")) //"a(b(c)d)"
 }
 
 func minimumRemovalsForValidParenthesis(input string) int {
@@ -53,4 +60,74 @@ func minimumRemovalsForValidParenthesis(input string) int {
 	}
 	//un-matching ")" + whatever left in the stack of un-matching "("
 	return count + stack.Size()
+}
+
+/**
+ A similar problem on leetcode
+  	https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+
+	Given a string s of '(' , ')' and lowercase English characters.
+
+	Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+	Formally, a parentheses string is valid if and only if:
+
+	It is the empty string, contains only lowercase characters, or
+	It can be written as AB (A concatenated with B), where A and B are valid strings, or
+	It can be written as (A), where A is a valid string.
+
+
+	Example 1:
+
+	Input: s = "lee(t(c)o)de)"
+	Output: "lee(t(c)o)de"
+	Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+	Example 2:
+
+	Input: s = "a)b(c)d"
+	Output: "ab(c)d"
+	Example 3:
+
+	Input: s = "))(("
+	Output: ""
+	Explanation: An empty string is also valid.
+	Example 4:
+
+	Input: s = "(a(b(c)d)"
+	Output: "a(b(c)d)"
+ */
+func leetcode_minimumRemovalsForValidParenthesis(input string) string {
+
+	//an empty string is also considered valid
+	if len(input) == 0 { return "" }
+
+	var stack Stack
+	indexList := make(map[int]bool)
+
+	for index, char := range input {
+		item := string(char)
+		if item == "(" {
+			stack.Push(index)
+		} else if item == ")" {
+			_, success := stack.Pop()
+			if !success  {
+				indexList[index] = true
+			}
+		} else {
+			fmt.Println(">>>> normal english letters, skipping...")
+		}
+	}
+
+	for !stack.IsEmpty() {
+		item, _ := stack.Pop()
+		indexList[item.(int)] = true
+	}
+	//Create the new string by ignoring the character in the string with identified index
+	result := ""
+	for index, char := range input {
+		if !indexList[index] {
+			result += string(char)
+		}
+	}
+	return result
 }
