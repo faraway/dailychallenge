3281e62cd3a4a5266fa1c2aaaef182a9ec703344
faraway <patrickyyao@gmail.com>
2021-04-16 23:03:38 -0700

    Add heap to utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "fmt"
+import (
+	"container/heap"
+	"fmt"
+)
 
 //package init function is optional, no parameter, no return value
 func init() {
@@ -71,14 +74,51 @@ func (s *Stack) Size() int {
 	return len(*s)
 }
 
+//-------------------------------Interval-----------------------------------
+type Interval struct {
+	Start int
+	End int
+}
+
+//-------------------------------IntHeap-----------------------------------
+//from https://golang.org/pkg/container/heap/
+//This IntHeap is a *min* heap of ints.
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
 
+//Note that Push and Pop in this interface are for package "container/heap" implementation to call.
+//To add and remove things from the heap, use heap.Push and heap.Pop.
+//See TestHeap() below
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
 
-
-
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 
 //-------------------------------Test Func-----------------------------------
 func TestFunc() {
 	fmt.Println("calling package function...")
+}
+
+func TestHeap() {
+	h := &IntHeap{2, 1, 5}
+	heap.Init(h)
+	heap.Push(h, 3)
+	fmt.Printf("minimum: %d\n", (*h)[0])
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
 }
