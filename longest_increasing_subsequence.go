e30584048fc2654882add7f30eb07e53c7d2511d
faraway <patrickyyao@gmail.com>
2021-04-18 22:31:16 -0700

    LIS dp solution

--- longest_increasing_subsequence.go
+++ longest_increasing_subsequence.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /**
 Longest Increasing Subsequence (LIS)
@@ -40,13 +43,22 @@ func main() {
 	fmt.Println("Answer is:", lengthOfLIS_bruteforce([]int{10,9,2,5,3,7,101,18})) //4
 	fmt.Println("Answer is:", lengthOfLIS_bruteforce([]int{0,1,0,3,2,3})) //4
 	fmt.Println("Answer is:", lengthOfLIS_bruteforce([]int{7,7,7,7,7,7,7})) //1
+
+	fmt.Println("Answer is:", lengthOfLIS([]int{10,9,2,5,3,7,101,18})) //4
+	fmt.Println("Answer is:", lengthOfLIS([]int{0,1,0,3,2,3})) //4
+	fmt.Println("Answer is:", lengthOfLIS([]int{7,7,7,7,7,7,7})) //1
 }
 
 /**
  Brute force algorithm, time complexity can be up to 2^n, where n is the length of input array
  */
+type Option struct {
+	Length int
+	Max int
+}
+
 func lengthOfLIS_bruteforce(nums []int) int {
-	options := []Option {{0, -9999}}
+	options := []Option {{0, math.MinInt32}}
 	for _, num := range nums {
 		var newOptions []Option
 		for _, option := range options {
@@ -66,7 +78,37 @@ func lengthOfLIS_bruteforce(nums []int) int {
 	return result
 }
 
-type Option struct {
-	Length int
-	Max int
+/**
+Improved solution, but kinda a improvement over the brute force solution above
+*/
+func lengthOfLIS(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+	/**
+		result is the array for DP.
+		result[i] means the smallest possible value for subsequence of length i
+		result[0] set to min value to get an easy start
+	 */
+	result := []int{math.MinInt32}
+	for _, num := range nums {
+		currLen := len(result)
+		//if current number > the last number of current sequence, then we can extend the sequence length by 1
+		if num > result[currLen-1] {
+			result = append(result, num)
+		}
+		//update the result array. If current number is larger than last value of sequence length=i-1 and smaller than last value of sequence length=i
+		//then last value of sequence length=i can be updated to current number.
+		for i := currLen-2; i>=0; i-- {
+			if num > result[i] && num < result[i+1] {
+				result[i+1] = num
+			}
+		}
+	}
+
+	return len(result) - 1
 }
+
+//TODO - read, another DP but slightly different solution:
+//https://github.com/faraway/fucking-algorithm/blob/master/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E8%AE%BE%E8%AE%A1%EF%BC%9A%E6%9C%80%E9%95%BF%E9%80%92%E5%A2%9E%E5%AD%90%E5%BA%8F%E5%88%97.md
