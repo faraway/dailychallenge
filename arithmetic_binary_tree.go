4c3476998db74aeedc9af3ea9864dea70009ecf5
faraway <patrickyyao@gmail.com>
2021-04-21 00:03:58 -0700

    arithmetic binary tree, add more test case

--- arithmetic_binary_tree.go
+++ arithmetic_binary_tree.go
@@ -28,6 +28,13 @@ This is a representation of the expression (3 + 2) * (4 + 5), and should return 
 func main() {
 	fmt.Println("initializing test data...")
 
+	/**
+				*
+			   / \
+			  +    +
+			 / \  / \
+			3  2  4  5
+	 */
 	root := &TreeNodeStr{Value: "*"}
 	root.Left = &TreeNodeStr{Value: "+"}
 	root.Right = &TreeNodeStr{Value: "+"}
@@ -40,6 +47,29 @@ func main() {
 
 	fmt.Println("Answer is:", evaluateArithmeticExpression(root)) //45
 
+	/**
+				+
+			   / \
+			  *    -
+			 / \  / \
+			5  4 100 /
+					/ \
+	               20  2
+	*/
+	root2 := &TreeNodeStr{Value: "+"}
+	root2.Left = &TreeNodeStr{Value: "*"}
+	root2.Right = &TreeNodeStr{Value: "-"}
+
+	root2.Left.Left = &TreeNodeStr{Value: "5"}
+	root2.Left.Right = &TreeNodeStr{Value: "4"}
+
+	root2.Right.Left = &TreeNodeStr{Value: "100"}
+	root2.Right.Right = &TreeNodeStr{Value: "/"}
+
+	root2.Right.Right.Left = &TreeNodeStr{Value: "20"}
+	root2.Right.Right.Right = &TreeNodeStr{Value: "2"}
+	fmt.Println("Answer is:", evaluateArithmeticExpression(root2)) //110
+
 }
 
 func evaluateArithmeticExpression(root *TreeNodeStr) int {
@@ -47,14 +77,20 @@ func evaluateArithmeticExpression(root *TreeNodeStr) int {
 		i, _ := strconv.Atoi(root.Value)
 		return i
 	}
+
 	left := evaluateArithmeticExpression(root.Left)
 	right := evaluateArithmeticExpression(root.Right)
+
 	var result int
 	switch root.Value {
-	case "+": result = left + right
-	case "-": result = left - right
-	case "*": result = left * right
-	case "/": result = left / right
+	case "+":
+		result = left + right
+	case "-":
+		result = left - right
+	case "*":
+		result = left * right
+	case "/":
+		result = left / right
 	}
 	return result
 }
