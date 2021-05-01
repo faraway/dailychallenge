a31152e4a58e802bd2bc768ff3c71aa1763cf086
faraway <patrickyyao@gmail.com>
2021-04-30 23:46:39 -0700

    add set in utils

--- utils/utils.go
+++ utils/utils.go
@@ -82,6 +82,38 @@ func (s *Stack) Size() int {
 	return len(*s)
 }
 
+//-------------------------------Set-----------------------------------
+type Set struct {
+	list map[int]struct{} //empty structs occupy 0 memory
+}
+
+func (s *Set) Has(v int) bool {
+	_, ok := s.list[v]
+	return ok
+}
+
+func (s *Set) Add(v int) {
+	s.list[v] = struct{}{}
+}
+
+func (s *Set) Remove(v int) {
+	delete(s.list, v)
+}
+
+func (s *Set) Clear() {
+	s.list = make(map[int]struct{})
+}
+
+func (s *Set) Size() int {
+	return len(s.list)
+}
+
+func NewSet() *Set {
+	s := &Set{}
+	s.list = make(map[int]struct{})
+	return s
+}
+
 //-------------------------------Interval-----------------------------------
 type Interval struct {
 	Start int
