b5e80443e811348d1a23cf7316520007cef692d6
faraway <patrickyyao@gmail.com>
2021-05-16 00:25:24 -0700

    Fix longest substring without repeating chars

--- main/longest_substring_without_repeating_characters.go
+++ main/longest_substring_without_repeating_characters.go
@@ -47,28 +47,34 @@ func main() {
 	fmt.Println("Answer is:", lengthOfLongestSubstring("dvdf")) //3
 }
 
-
+/**
+Using sliding window solution (typical case for "substring" problems)
+See: https://github.com/labuladong/fucking-algorithm/blob/master/%E7%AE%97%E6%B3%95%E6%80%9D%E7%BB%B4%E7%B3%BB%E5%88%97/%E6%BB%91%E5%8A%A8%E7%AA%97%E5%8F%A3%E6%8A%80%E5%B7%A7.md
+ */
 func lengthOfLongestSubstring(input string) int {
-	checkMap := make(map[int32]bool)
-	maxLength, currentLength := 0, 0
+	dupChecker := make(map[uint8]bool)
+	maxLength := 0
+	left, right := 0, 0
 
-	for _, char := range input {
-		//fmt.Println(idx, "=>", string(char))
-		if !checkMap[char] {
-			currentLength ++
-			checkMap[char] = true
-		} else {
-			//clear the map, or maybe just create a new map. i.e. checkMap := make(map[int32]bool)
-			for k := range checkMap {
-				delete(checkMap, k)
+	for right < len(input) {
+
+		for right < len(input) && !dupChecker[input[right]] {
+			dupChecker[input[right]] = true
+			right++
+			if (right - left) > maxLength {
+				maxLength = right - left
 			}
-			currentLength = 1
-			checkMap[char] = true
+		}
+		for left < right && right < len(input) {
+			dupChecker[input[left]] = false
+			left++
+			//this means we just moved over the char that's duplicate of "right" pointer has seen;
+			//break out of look
+			if input[left-1] == input[right] {
+				break
+			}
 		}
 
-		if currentLength > maxLength {
-			maxLength = currentLength
-		}
 	}
 	return maxLength
 }
