0cd3b6cdbce569bd29a928b421d5024d54f381a2
faraway <patrickyyao@gmail.com>
2021-08-28 22:06:15 -0700

    add union by rank and path compression optimization for disjoint set

--- utils/disjoint_set.go
+++ utils/disjoint_set.go
@@ -1,27 +1,49 @@
 package utils
 
-type DisjointSet []int
+type DisjointSet struct {
+	Parent []int
+	Rank   []int
+}
 
 func NewDisjointSet(size int) DisjointSet {
-	ds := make([]int, size)
-	for i,_ := range ds {
-		ds[i] = i
+	ds := DisjointSet{
+		Parent: make([]int, size),
+		Rank:   make([]int, size),
+	}
+	for i,_ := range ds.Parent {
+		ds.Parent[i] = i
+		ds.Rank[i] = 1
 	}
 	return ds
 }
 
+/**
+ Find with path compression
+ (note the recursion)
+ */
 func (d DisjointSet) Find(x int) int {
-	for d[x] != x {
-		x = d[x]
+	for d.Parent[x] == x {
+		return x
 	}
-	return x
+	d.Parent[x] = d.Find(d.Parent[x])
+	return d.Parent[x]
 }
 
+/**
+ Union with rank optimization
+ */
 func (d DisjointSet) Union(x int, y int) {
 	rootX := d.Find(x)
 	rootY := d.Find(y)
 	if rootX != rootY {
-		d[rootY] = rootX
+		if d.Rank[rootX] > d.Rank[rootY] {
+			d.Parent[rootY] = rootX
+		} else if d.Rank[rootX] < d.Rank[rootY] {
+			d.Parent[rootX] = rootY
+		} else { //same height, pick one to be the child of the other, but need to update the rank
+			d.Parent[rootY] = rootX
+			d.Rank[rootX] += 1
+		}
 	}
 }
 
